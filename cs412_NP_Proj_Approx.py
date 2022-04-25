b8c9f376168ea581e170865982a83e1d598cc939
Hafeta00 <72397859+Hafeta00@users.noreply.github.com>
2022-04-25 11:59:39 -0400

    Add files via upload

---
+++ cs412_NP_Proj_Approx.py
@@ -0,0 +1,16 @@
+"""
+    name:  Hafet Abdulle, Sammy Collins, Matt Andrews, Kory Erdmann
+
+"""
+# All modules for CS 412 must include a main method that allows it
+# to imported and invoked from other python scripts
+from collections import deque
+
+
+def main():
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
