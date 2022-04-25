0f223befd0fdb55b93433019bc01d10db6037fc1
Matthew A. Andrews <andre2ma@dukes.jmu.edu>
2022-04-25 12:20:26 -0400

    Boilerplate

--- cs412_NP_Proj_Exact.py
+++ cs412_NP_Proj_Exact.py
@@ -1,1 +1,14 @@
+"""
+Matt Andrews
+"""
 
+
+def main():
+    vertex_num - int(input())
+    adj_list = {x: None for x in range(vertex_num)}
+    for i in range(vertex_num):
+        pass
+    
+    
+if __name__ == "__main__":
+    main()
