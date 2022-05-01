9ccf147f17d46b836877b580f2da87dfbd9117d2
Matthew A. Andrews <andre2ma@dukes.jmu.edu>
2022-05-01 15:05:46 -0400

    Test cases

--- exact_solution/test_cases/adj_list_generator.py
+++ exact_solution/test_cases/adj_list_generator.py
@@ -1,8 +1,8 @@
 """
 Matt Andrews
-random_graph function from:
+random_graph function by kaya3 from:
 https://stackoverflow.com/questions/60161376/easiest-way-to-generate-an-undirected-graph-s-adjacency-list
-with slight alterations and comments
+with slight alterations, comments, and main() by Matt Andrews
 """
 from random import random
 from itertools import product, combinations
@@ -14,12 +14,10 @@ def random_graph(n, p, *, directed=False):
     adj_list = {i: [] for i in nodes}
     # Set of all 2-node subsets
     possible_edges = product(nodes, repeat=2) if directed else combinations(nodes, 2)
-    # Select edge based on probability p
-    # Add the corresponding edge for undirected graphs
     for u, v in possible_edges:
-        if random() < p:
+        if random() < p:  # Select edge based on probability p
             adj_list[u].append(v)
-            if not directed:
+            if not directed: # Add the corresponding edge for undirected graphs
                 adj_list[v].append(u)
     return adj_list
 
--- exact_solution/test_cases/run_test_cases.sh
+++ exact_solution/test_cases/run_test_cases.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-python ../cs412_NP_Proj_Exact.py < small_adj_list_0.txt > small_solution_0.txt
-python ../cs412_NP_Proj_Exact.py < small_adj_list_1.txt > small_solution_1.txt
-python ../cs412_NP_Proj_Exact.py < medium_adj_list_0.txt > medium_solution_0.txt
+python ../cs412_NP_Proj_Exact.py < small_adj_list_0.txt > small_solution_0.txt # 5 nodes
+python ../cs412_NP_Proj_Exact.py < small_adj_list_1.txt > small_solution_1.txt # 10 nodes
+python ../cs412_NP_Proj_Exact.py < medium_adj_list_0.txt > medium_solution_0.txt # 100 nodes
+python ../cs412_NP_Proj_Exact.py < medium_adj_list_1.txt > medium_solution_1.txt # 1000 nodes ~10 seconds
+python ../cs412_NP_Proj_Exact.py < medium_adj_list_2.txt > medium_solution_2.txt # 5000 nodes ~13 minutes
+python ../cs412_NP_Proj_Exact.py < medium_adj_list_3.txt > medium_solution_3.txt # 2000 nodes ~55 seconds
+python ../cs412_NP_Proj_Exact.py < medium_adj_list_4.txt > medium_solution_4.txt # 3000 nodes ~3 minutes
+python ../cs412_NP_Proj_Exact.py < large_adj_list_0.txt > large_solution_0.txt # This test will run a long time - 10 000 nodes
