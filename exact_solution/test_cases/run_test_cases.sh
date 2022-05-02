7df543bac20c8b98741655126a1732a7ad426fe4
Matt Andrews <33872177+Snugturtle@users.noreply.github.com>
2022-05-01 20:43:35 -0400

    runtimes

--- exact_solution/test_cases/run_test_cases.sh
+++ exact_solution/test_cases/run_test_cases.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-python ../cs412_NP_Proj_Exact.py < small_adj_list_0.txt > small_solution_0.txt # 5 nodes
-python ../cs412_NP_Proj_Exact.py < small_adj_list_1.txt > small_solution_1.txt # 10 nodes
-python ../cs412_NP_Proj_Exact.py < medium_adj_list_0.txt > medium_solution_0.txt # 100 nodes
+python ../cs412_NP_Proj_Exact.py < small_adj_list_0.txt > small_solution_0.txt # 5 nodes      < 1 sec
+python ../cs412_NP_Proj_Exact.py < small_adj_list_1.txt > small_solution_1.txt # 10 nodes     < 1 sec
+python ../cs412_NP_Proj_Exact.py < medium_adj_list_0.txt > medium_solution_0.txt # 100 nodes  < 1 sec
 python ../cs412_NP_Proj_Exact.py < medium_adj_list_1.txt > medium_solution_1.txt # 1000 nodes ~10 seconds
 python ../cs412_NP_Proj_Exact.py < medium_adj_list_2.txt > medium_solution_2.txt # 5000 nodes ~13 minutes
 python ../cs412_NP_Proj_Exact.py < medium_adj_list_3.txt > medium_solution_3.txt # 2000 nodes ~55 seconds
